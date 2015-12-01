2de4a2afb3d0b00922c7b2565a40af069487a23f
NicoD-NITH <nicko_9289@hotmail.com>
2015-12-01 12:10:37 +0100

    lagt til slideshow men trengs litt til fortsatt

--- app.js
+++ app.js
@@ -23,3 +23,19 @@ function addPicture(url) {
     $('section.pictures img:last').after(html);
     $('.add-picture-url').val('');
 }
+//Variable for slideshow:
+var count;
+var imageArray = new Array();
+
+
+//Slideshow function:
+function slideshow(){
+    if(!document.images)
+        return;
+    document.getElementById('slide').src = imageArray[count].src
+    if(count<2)
+        count++
+    else
+        step=0;
+        setTimeout("slideshow()",2500);
+}
